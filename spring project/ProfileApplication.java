6c017fe5b3c6c8051766a16513f0cb825746b441
Subashgnanavel <146078440+Subashgnanavel@users.noreply.github.com>
2024-08-27 08:41:09 +0100

    Add files via upload

---
+++ spring project/ProfileApplication.java
@@ -0,0 +1,5 @@
+package com.file.profile;
+
+public class ProfileApplication {
+    
+}
---
+++ spring project/Repository.java
@@ -0,0 +1,7 @@
+package com.file.profile;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Repository extends JpaRepository<StudModel,Integer> {
+    
+}
